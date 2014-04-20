957a9bf7e8850a358e80e0aaa626b74645c2933e
Austin Hesterly <austin.hesterly@gmail.com>
2014-04-20 01:06:55 -0600

    Completed 4 total videos for both Standing pages

--- js/jqStyleMain.js
+++ js/jqStyleMain.js
@@ -1,7 +1,9 @@
-$(document). ready(function(){
+$(document).ready(function(){
 	//When an icon is clicked, create slide out menu
 	$('.initGrid').click(function(){
 		$(this).children().slideToggle('slow');
 	});
-
+	$('.videos').mouseenter(function(){
+		$(this).css("opacity", "1");
+	})
 });
--- menus/Standing/Throws/jqStyleMain.js
+++
@@ -1,9 +0,0 @@
-$(document). ready(function(){
-	//When an icon is clicked, create slide out menu
-	$('.initGrid').click(function(){
-		$(this).children().slideToggle('slow');
-	});
-	// $('.initGrid').click(function(){
-	// 	$(this).children().slideUp('slow');
-	// });
-});
